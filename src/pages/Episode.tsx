353f6f884d20d4cd2d383e58c97cd7a3ed263f93
Nathan Jensen <nate@Nathans-MacBook-Pro.local>
2023-12-12 17:59:10 +0100

    fix indentation

--- src/pages/Episode.tsx
+++ src/pages/Episode.tsx
@@ -7,30 +7,29 @@ import { x } from "@xstyled/styled-components";
 import ShowDisplay from "../components/ShowDisplay";
 
 export default function Episode(){
-    let location = useLocation();
-    const show = useSelector<RootState, TVShowState>((state) => state.tvShow);
-  
-    const selected = show.episodes.find(e => e.id === location.state.episodeID);
-		
-    if(!selected){
-        return <div></div>
-    }
+	let location = useLocation();
+	const show = useSelector<RootState, TVShowState>((state) => state.tvShow);
 
-    const image = selected?.image?.original || selected?.image?.medium || show.imgUrl;
+	const selected = show.episodes.find(e => e.id === location.state.episodeID);
+	
+	if(!selected){
+			return <div></div>
+	}
 
-    return (
-        <>
-            <x.div bg="black" padding={8}>  
-                <x.div marginBottom={8}>            
-                    <StyledLink to="/" text="Go Back"/>
-                </x.div>
-                <x.div display="flex">
-                    <ShowDisplay name={selected.name} summary={selected.summary} imgUrl={image}/>
-                </x.div>
-            
-            </x.div>
-            <x.div bg="black" display="flex" minHeight="50vh">
-            </x.div>
-        </>
-    )
+	const image = selected?.image?.original || selected?.image?.medium || show.imgUrl;
+
+	return (
+		<>
+			<x.div bg="black" padding={8}>  
+				<x.div marginBottom={8}>            
+						<StyledLink to="/" text="Go Back"/>
+				</x.div>
+				<x.div display="flex">
+						<ShowDisplay name={selected.name} summary={selected.summary} imgUrl={image}/>
+				</x.div>
+			</x.div>
+			<x.div bg="black" display="flex" minHeight="50vh">
+			</x.div>
+		</>
+	)
 }
