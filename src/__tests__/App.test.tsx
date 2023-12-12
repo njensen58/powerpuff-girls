6f0d165262da3ced196ffce53418b5716780f475
Nathan Jensen <nate@Nathans-MacBook-Pro.local>
2023-12-12 13:13:07 +0100

    add auto prettier on commit

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,62 +1,44 @@
-import { screen, act } from '@testing-library/react';
-import { renderWithProviders } from '../utils/test-utils';
-import { setupStore } from '../store';
-import { fetchTVShow } from '../store/tvShowSlice';
-import App from '../App';
-
-
-beforeEach(() => {
-  // @ts-expect-error - partial response for testing
-  jest.spyOn(global, 'fetch').mockResolvedValue({
-      json: jest.fn().mockResolvedValue({
-        name: "test",
-        summary: "test",
-        image: {
-          original: "test",
-          medium: "test"
-        },
-        _embedded: {episodes: []},
-        isLoading: false,
-        errorMsg: "",
-      })
-  })
-})
+import { screen, act } from "@testing-library/react";
+import { renderWithProviders } from "../utils/test-utils";
+import { setupStore } from "../store";
+import { fetchTVShow } from "../store/tvShowSlice";
+import App from "../App";
 
 afterEach(() => jest.restoreAllMocks());
 
 describe("App", () => {
-  test('renders hello world', () => {
+  test("renders hello world", () => {
     renderWithProviders(<App />);
     const headerElement = screen.getByText(/hello world/i);
     expect(headerElement).toBeInTheDocument();
   });
 
-  test('it renders response data from api call on load', async () => {
+  test("it renders response data from api call on load", async () => {
     // @ts-expect-error - partial response for testing
-    jest.spyOn(global, 'fetch').mockResolvedValue({
+    jest.spyOn(global, "fetch").mockResolvedValue({
       json: jest.fn().mockResolvedValue({
         name: "Powerpuff girls",
         summary: "test",
         image: {
           original: "test",
-          medium: "test"
+          medium: "test",
         },
-        _embedded: {episodes: []},
+        _embedded: { episodes: [] },
         isLoading: false,
         errorMsg: "",
-      })
-    })
+      }),
+    });
 
     let store;
 
     await act(async () => {
       store = setupStore();
       await store.dispatch(fetchTVShow("powerpuff+girls"));
-    })
+    });
 
-    renderWithProviders(<App />, {store});
+    renderWithProviders(<App />, { store });
 
     const title = screen.getByText(/Powerpuff girls/i);
-    expect(title).toBeInTheDocument()
-  }); 
+    expect(title).toBeInTheDocument();
+  });
 });
